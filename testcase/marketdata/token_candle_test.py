f2292388abe30946bd9864e39d95947c8af6d3a7
xuruizeng <xuruizeng@laplacetech.cn>
2018-07-17 18:36:03 +0800

    edit testcase of token_candle

--- testcase/marketdata/token_candle_test.py
+++ testcase/marketdata/token_candle_test.py
@@ -8,7 +8,7 @@ from common.request_data import ConfigRequest
 
 config = GetConfig()
 base_url = config.get_url('Base_Url')
-test_url = config.get_url('token_quotation_ranking_change_ratio_24h')
+test_url = config.get_url('token_candle')
 
 log = MyLog.get_log()
 logger = log.get_logger()
@@ -30,8 +30,27 @@ class TokenCandle(unittest.TestCase):
         测试返回数据的正确性
         """
         request = ConfigRequest()
-        request.set_url(test_url)
+        request.set_url(test_url.format('Bitcoin', '1d'))
         response = request.get()
 
         self.assertIsInstance(response, list)
         self.assertGreater(len(response), 0)
+
+        for item in response:
+            self.assertIsInstance(item, list)
+            self.assertEqual(len(item), 17)
+            # time
+            self.assertIsInstance(item[0], int)
+            self.assertGreater(item[0], 0)
+            # open
+            self.assertIn(type(item[1]), [float, int])
+            self.assertGreater(item[1], 0)
+            # close
+            self.assertIn(type(item[2]), [float, int])
+            self.assertGreater(item[2], 0)
+            # low
+            self.assertIn(type(item[3]), [float, int])
+            self.assertGreater(item[3], 0)
+            # high
+            self.assertIn(type(item[4]), [float, int])
+            self.assertGreater(item[4], 0)
